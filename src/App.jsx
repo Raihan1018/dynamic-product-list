c67be36b630d5f7d5328e9c5fa5b6bc9b706f580
Raihan <raihanuddinahmed10@gmail.com>
2025-09-01 23:48:47 +0600

    single product UI done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import SingleProducts from "./components/SingleProducts";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
-    <div>App</div>
-  )
-}
+    <div className="p-5 grid-cols-12 flex justify-between">
+      {/* main content */}
+      <div className="grid-cols-6 ">
+        <h2>Total Products: {products.length}</h2>
+        {products.map((product) => (
+          <SingleProducts key={product.id} product={product} />
+        ))}
+      </div>
+      {/* cart */}
+      <div className="grid-cols-6 bg-red-700">
+        <h3>cart</h3>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
